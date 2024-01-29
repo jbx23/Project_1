ef699a1b01c12ea5addd37a08c94c3822d3e1aae
jbx23 <22953358@student.uwa.edu.au>
2024-01-30 00:34:15 +0700

    Update settings.py

--- JPad_Project/JPad_Project/settings.py
+++ JPad_Project/JPad_Project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,15 +77,22 @@ WSGI_APPLICATION = 'JPad_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#      "default": {
+#         "ENGINE": 'django.db.backends.postgresql',
+#         "NAME": 'JPad_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-     "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        "NAME": 'JPad_db',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    'OPTIONS': {
+            'init_command': "SET TIME ZONE 'Australia/Perth';",
+        },
 }
 
 
@@ -128,8 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 # Additional locations of static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Add this line
-
 ]
+
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
