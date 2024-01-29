863f8316a451c288c0956a4c08c9b01edb3087a2
jbx23 <22953358@student.uwa.edu.au>
2024-01-29 21:03:37 +0700

    Update settings.py

--- JPad_Project/JPad_Project/settings.py
+++ JPad_Project/JPad_Project/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEBUG = True
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
